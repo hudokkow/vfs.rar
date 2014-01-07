c63090e1357d93355cbf3f528ca5083537171051
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-01-07 16:56:20 +0100

    s

--- src/RarFile.cpp
+++ src/RarFile.cpp
@@ -739,7 +739,8 @@ bool DirectoryExists(VFSURL* url)
   return false;
 }
 
-void* GetDirectory(VFSURL* url, VFSDirEntry** items, int* num_items)
+void* GetDirectory(VFSURL* url, VFSDirEntry** items,
+                   int* num_items, VFSCallbacks* callbacks)
 {
   std::string strPath(url->url);
   size_t pos;
